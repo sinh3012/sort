d30b78dad01b4ed184ce300e68c41d91c41f57cb
Lair Sinh <assassin3012@mail.ru>
2016-11-14 17:07:25 +0400

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -3,4 +3,14 @@
 
 SCENARIO("Init", "[Init]"){
   mysort();
+  std::ifstream file1("sorted.txt");
+  if (!file1.is_open()) throw("no_file");
+  std::ifstream file2("sorted1.txt");
+  if (!file1.is_open()) throw("no_file");
+  std::string str1;
+  std::string str2;
+  for (size_t t = 0; t < 800; ++t)
+    std::getline(file1, str1);
+    std::getline(file2, str2);
+    REQUIRE(str1 == str2);
 }

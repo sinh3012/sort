8d07be4cf8438df37eab05f2a3e7de3b0f69db0e
Lair Sinh <assassin3012@mail.ru>
2016-11-17 01:16:38 +0300

    Update sort.hpp

--- include/sort.hpp
+++ include/sort.hpp
@@ -1,3 +1,4 @@
+#include "stdafx.h"
 #include <string>
 #include <vector>
 #include <fstream>
@@ -17,130 +18,130 @@ auto toString(T value) -> std::string
 
 auto byString(const std::string& a, std::string& b) -> bool { return a < b; }
 
-auto piece(std::string const name, size_t count_, size_t memory_) -> void
+auto piece(std::string const name, size_t memory_) -> size_t
 {
 	std::ifstream file(name);
 	if (!file.is_open()) throw("no_file");
 	std::ofstream tempfile;
 	std::string temp;
 	std::vector<std::string> str;
-	for (size_t t = 0; t < count_; ++t) {
-		tempfile.open((toString(t + memory_ * 100)));
+	size_t t = 0;
+	while (!file.eof()) {
+		tempfile.open((toString(t + memory_ * 100000)));
 		for (size_t tt = 0; tt < memory_; ++tt) {
+			if (file.eof()) break;
 			std::getline(file, temp);
 			str.push_back(temp);
 		}
 		std::sort(str.begin(), str.end(), byString);
-		for (auto i : str) {
-			tempfile << i << std::endl;
+		for (size_t tt = 0; tt < str.size() - 1; ++tt) {
+			tempfile << str[tt] << std::endl;
 		}
+		tempfile << str[str.size() - 1];
 		str.clear();
 		tempfile.close();
+		++t;
 	}
 	file.close();
+	return t;
 }
 
-auto merge2(std::string strfile1, std::string strfile2, std::string strtempfile, size_t mem1, size_t mem2) -> void
+auto merge2(std::string strfile1, std::string strfile2, std::string strtempfile) -> void
 {
 	std::ifstream file1(strfile1);
 	std::ifstream file2(strfile2);
-	std::ofstream tempfile(strtempfile);
+	std::ofstream tempfile("temp.txt");
 	std::string temp1;
 	std::string temp2;
-	size_t t1 = mem1, t2 = mem2;
 	std::getline(file1, temp1);
 	std::getline(file2, temp2);
-	while (true) {
+	bool k1 = true, k2 = true;
+	while (k1 && k2) {
 		if (byString(temp1, temp2)) {
 			tempfile << temp1 << std::endl;
-			if (--t1) {
+			k1 = false;
+			if (!file1.eof()) {
 				std::getline(file1, temp1);
+				k1 = true;
 			}
-			else break;
 		}
 		else {
 			tempfile << temp2 << std::endl;
-			if (--t2) {
+			k2 = false;
+			if (!file2.eof()) {
 				std::getline(file2, temp2);
+				k2 = true;
 			}
-			else break;
 		}
 	}
-	while (t1 > 0) {
-		tempfile << temp1 << std::endl;
-		std::getline(file1, temp1);
-		--t1;
+	while (k1) {
+		tempfile << temp1;
+		k1 = false;
+		if (!file1.eof()) {
+			tempfile << std::endl;
+			std::getline(file1, temp1);
+			k1 = true;
+		}
 	}
-	while (t2 > 0) {
-		tempfile << temp2 << std::endl;
-		std::getline(file2, temp2);
-		--t2;
+	while (k2) {
+		tempfile << temp2;
+		k2 = false;
+		if (!file2.eof()) {
+			tempfile << std::endl;
+			std::getline(file2, temp2);
+			k2 = true;
+		}
 	}
 	file1.close();
 	file2.close();
 	remove(strfile1.c_str());
 	remove(strfile2.c_str());
+	remove(strtempfile.c_str());
 	tempfile.close();
+	rename("temp.txt", strtempfile.c_str());
 }
-/*void merge(size_t count, std::string strtempfile, size_t mem) {
-std::ifstream *p = new std::ifstream[count];
-std::string *ps = new std::string[count];
-size_t *k = new size_t[count];
-for (size_t t = 0; t < count; ++t) {
-p[t].open(toString(t + count * 100));
-std::getline(p[t], ps[t]);
-k[t] = mem;
-}
-std::ofstream tempfile(strtempfile);
-while ( {
-}
-file1.close();
-file2.close();
-remove(strfile1.c_str());
-remove(strfile2.c_str());
-tempfile.close();
-}
-*/
-auto allsort(std::string filename_, size_t count_, size_t memory_) -> void
+
+auto allsort(std::string filename_, size_t memory_) -> void
 {
-	piece(filename_, count_, memory_);
-	size_t num_ = (count_ / 2) * 2;
+
+	size_t num_ = piece(filename_, memory_);
 	size_t mem_ = memory_;
 	while (num_ != 1) {
 		for (size_t t = 0; t < num_ / 2; ++t) {
-			merge2(toString(mem_ * 100 + 2 * t), toString(mem_ * 100 + 2 * t + 1), toString(t + mem_ * 200), mem_, mem_);
+			merge2(toString(mem_ * 100000 + 2 * t), toString(mem_ * 100000 + 2 * t + 1), toString(t + mem_ * 200000));
+		}
+		if (num_ % 2 == 1) {
+			merge2(toString(mem_ * 200000), toString(mem_ * 100000 + num_ - 1), toString(mem_ * 200000));
 		}
 		mem_ *= 2;
 		num_ /= 2;
 	}
-	if (count_ % 2 == 1) {
-		merge2(toString(100 * mem_), toString(100 * memory_ + count_ - 1), "sorted.txt", mem_, memory_);
-	}
-	else {
-		remove("sorted.txt");
-		rename(toString(mem_ * 100).c_str(), "sorted.txt");
-	}
+	remove("sorted.txt");
+	rename(toString(mem_ * 100000).c_str(), "sorted.txt");
 }
+
 void main(int argc, char* argv[])
 {
 	//SetConsoleCP(1251);
 	//SetConsoleOutputCP(1251);
-	if (argc == 4) {
-		allsort(static_cast<std::string>(argv[1]), std::atoi(argv[2]) / std::atoi(argv[3]), std::atoi(argv[3]));
+	if (argc == 3) {
+		allsort(static_cast<std::string>(argv[1]), std::atoi(argv[2]));
 		system("pause");
 	}
-	else if (argc == 5 && argv[1] == "test") {
+	else if (argc == 4 && static_cast<std::string>(argv[1]) == "test") {
 		std::string filename1 = argv[2];
 		std::string filename2 = argv[3];
 		std::ifstream file1(filename1);
 		std::ifstream file2(filename2);
 		std::string temp1;
 		std::string temp2;
-		for (int i = 0; i < std::atoi(argv[4]); ++i) {
+		size_t t = 0;
+		while (true) {
+			if (file1.eof() || file2.eof()) break;
 			std::getline(file1, temp1);
 			std::getline(file2, temp2);
 			if (temp1 != temp2) {
-				std::cout << "Error " << i << temp1 << temp2 << std::endl;
+				std::cout << "Error " << t << " " << temp1 << " " << temp2 << std::endl;
 				system("pause");
 			}
 		}
@@ -151,4 +152,3 @@ void main(int argc, char* argv[])
 		system("pause");
 	}
 }
-

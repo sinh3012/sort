fa7240ea9667d7d2166eeac40cba01197b5f6256
Lair Sinh <assassin3012@mail.ru>
2016-11-20 00:44:31 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -4,7 +4,7 @@
 SCENARIO("mem213", "[mem213]"){
   allsort("1.txt", 3);
 	std::ifstream file1("sorted.txt");
-	std::ifstream file2("2.txt");
+	std::ifstream file2("3.txt");
 	REQUIRE(file1.is_open());
 	REQUIRE(file2.is_open());
 	std::string temp1;
@@ -14,7 +14,7 @@ SCENARIO("mem213", "[mem213]"){
 	while (!file1.eof() && !file2.eof()) {
 		std::getline(file1, temp1);
 		std::getline(file2, temp2);
-		REQUIRE(temp2 == "2");
+		REQUIRE(temp2 == "Holmes James 1991");
 		if (temp1 != temp2) {
 			REQUIRE(temp1 != temp2);
       			Q = false;

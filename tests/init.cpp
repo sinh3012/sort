5c456a12db0fb351e1540efd4c58b860aab55180
Lair Sinh <assassin3012@mail.ru>
2016-11-20 22:21:50 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -7,7 +7,6 @@ SCENARIO("mem213", "[mem213]"){
 	std::ifstream file2("4.txt");
 	std::string temp1;
 	std::string temp2;
-	rewind(file1);
 	bool Q = true;
 	size_t t = 0;
 	while (!file1.eof() && !file2.eof()) {

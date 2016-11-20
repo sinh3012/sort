9cc9003af5f17f078ff1c6bfac565420c61ae016
Lair Sinh <assassin3012@mail.ru>
2016-11-21 00:57:20 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -4,7 +4,7 @@
 SCENARIO("mem213", "[mem213]"){
   	allsort("name.txt", 2);
 	std::ifstream file1("sorted.txt");
-	std::ifstream file2("sorted_test.txt");
+	std::ifstream file2("name_test.txt");
 	std::string temp1;
 	std::string temp2;
 	bool Q = true;

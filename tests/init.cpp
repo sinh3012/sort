6082b5c6e00aec96aa51c6d2efa83dca47ee68f4
Lair Sinh <assassin3012@mail.ru>
2016-11-22 01:00:48 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -26,3 +26,81 @@ SCENARIO("memory_very_small", "memory_very_small]"){
   	file1.close();
   	file2.close();
 }
+
+SCENARIO("memory_small", "memory_small]"){
+  	allsort("name1.txt", 2);
+	std::ifstream file1("sorted.txt");
+	std::ifstream file2("name1_test.txt");
+	std::string temp1;
+	std::string temp2;
+	bool Q = true;
+	size_t t = 0;
+	//std::getline(file1, temp1);
+	std::getline(file2, temp2);
+	while (!file1.eof() && !file2.eof()) {
+		std::getline(file1, temp1);
+		std::getline(file2, temp2);
+		if (temp1 != temp2) {
+			REQUIRE(temp1 != temp2);
+      			Q = false;
+      			break;
+		}
+		++t;
+	}
+	REQUIRE(t == 1069553);
+  	REQUIRE(Q);
+  	file1.close();
+  	file2.close();
+}
+
+SCENARIO("memory_normal", "memory_normal]"){
+  	allsort("name1.txt", 4);
+	std::ifstream file1("sorted.txt");
+	std::ifstream file2("name1_test.txt");
+	std::string temp1;
+	std::string temp2;
+	bool Q = true;
+	size_t t = 0;
+	//std::getline(file1, temp1);
+	std::getline(file2, temp2);
+	while (!file1.eof() && !file2.eof()) {
+		std::getline(file1, temp1);
+		std::getline(file2, temp2);
+		if (temp1 != temp2) {
+			REQUIRE(temp1 != temp2);
+      			Q = false;
+      			break;
+		}
+		++t;
+	}
+	REQUIRE(t == 1069553);
+  	REQUIRE(Q);
+  	file1.close();
+  	file2.close();
+}
+
+SCENARIO("memory_big", "memory_big]"){
+  	allsort("name1.txt", 8);
+	std::ifstream file1("sorted.txt");
+	std::ifstream file2("name1_test.txt");
+	std::string temp1;
+	std::string temp2;
+	bool Q = true;
+	size_t t = 0;
+	//std::getline(file1, temp1);
+	std::getline(file2, temp2);
+	while (!file1.eof() && !file2.eof()) {
+		std::getline(file1, temp1);
+		std::getline(file2, temp2);
+		if (temp1 != temp2) {
+			REQUIRE(temp1 != temp2);
+      			Q = false;
+      			break;
+		}
+		++t;
+	}
+	REQUIRE(t == 1069553);
+  	REQUIRE(Q);
+  	file1.close();
+  	file2.close();
+}

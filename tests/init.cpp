2411e38f766502a719c148f74737a2f9f79a874b
Lair Sinh <assassin3012@mail.ru>
2016-11-27 18:04:59 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,29 +1,15 @@
 #include "sort.hpp"
 #include <catch.hpp>
 
-SCENARIO("memory_very_small", "memory_very_small]"){
-  	allsort("15", 4);
+SCENARIO("file_small", "file_small]"){
+  	allsort("8", 2);
 	std::ifstream file1("sorted.txt");
+	std::ifstream file2("8_test.txt");
 	std::string temp1;
+	std::string temp2;
+	bool Q = true;
 	size_t t = 0;
 	std::getline(file1, temp1);
-	while (!file1.eof()) {
-		std::getline(file1, temp1);
-		++t;
-	}
-	REQUIRE(t);
-  	file1.close();
-}
-/*
-SCENARIO("memory_small", "memory_small]"){
-  	allsort("name1.txt", 2);
-	std::ifstream file1("sorted.txt");
-	std::ifstream file2("name1_test.txt");
-	std::string temp1;
-	std::string temp2;
-	bool Q = true;
-	size_t t = 0;
-	//std::getline(file1, temp1);
 	std::getline(file2, temp2);
 	while (!file1.eof() && !file2.eof()) {
 		std::getline(file1, temp1);
@@ -35,21 +21,20 @@ SCENARIO("memory_small", "memory_small]"){
 		}
 		++t;
 	}
-	REQUIRE(t == 1069553);
+	REQUIRE(t == 422453);
   	REQUIRE(Q);
   	file1.close();
   	file2.close();
 }
-
-SCENARIO("memory_normal", "memory_normal]"){
-  	allsort("name1.txt", 4);
+SCENARIO("file_normal", "file_normal]"){
+  	allsort("15", 4);
 	std::ifstream file1("sorted.txt");
-	std::ifstream file2("name1_test.txt");
+	std::ifstream file2("15_test.txt");
 	std::string temp1;
 	std::string temp2;
 	bool Q = true;
 	size_t t = 0;
-	//std::getline(file1, temp1);
+	std::getline(file1, temp1);
 	std::getline(file2, temp2);
 	while (!file1.eof() && !file2.eof()) {
 		std::getline(file1, temp1);
@@ -61,13 +46,13 @@ SCENARIO("memory_normal", "memory_normal]"){
 		}
 		++t;
 	}
-	REQUIRE(t == 1069553);
+	REQUIRE(t == 422453);
   	REQUIRE(Q);
   	file1.close();
   	file2.close();
 }
 
-SCENARIO("memory_big", "memory_big]"){
+SCENARIO("memory_20", "memory_20]"){
   	allsort("name1.txt", 8);
 	std::ifstream file1("sorted.txt");
 	std::ifstream file2("name1_test.txt");
@@ -92,4 +77,3 @@ SCENARIO("memory_big", "memory_big]"){
   	file1.close();
   	file2.close();
 }
-*/

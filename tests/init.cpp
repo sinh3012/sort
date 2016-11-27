64ce2f2dd6bfff45757c7cbb7f8960b020cc0126
Lair Sinh <assassin3012@mail.ru>
2016-11-27 19:36:33 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,8 +1,8 @@
 #include "sort.hpp"
 #include <catch.hpp>
 
-  SCENARIO("memory_very_small", "memory_very_small]"){
-  	allsort("name1.txt", 1);
+/*SCENARIO("memory_very_small", "memory_very_small]"){
+	allsort("name1.txt", 1);
   	std::ifstream file1("sorted.txt");
   	std::string temp1;
   	size_t t = 0;
@@ -11,12 +11,12 @@
   		std::getline(file1, temp1);
   		++t;
   	}
-	REQUIRE(t == 1069553);
-    	file1.close();
-  }
-  /*
+	REQUIRE(t == 1069553+1);
+	file1.close();
+  
+}*/
 SCENARIO("file_small", "file_small]"){
-  	allsort("8", 2);
+  	allsort("8", 1);
 	std::ifstream file1("sorted.txt");
 	std::string temp1;
 	size_t t = 0;
@@ -28,7 +28,7 @@ SCENARIO("file_small", "file_small]"){
 		}
 		++t;
 	}
-	REQUIRE(t == 422453);
+	REQUIRE(t == 422453+1);
   	file1.close();
 }
 SCENARIO("file_normal", "file_normal]"){
@@ -44,11 +44,11 @@ SCENARIO("file_normal", "file_normal]"){
 		}
 		++t;
 	}
-	REQUIRE(t == 791948);
+	REQUIRE(t == 791948+1);
   	file1.close();
 }
 SCENARIO("file_big", "file_big]"){
-  	allsort("32", 8);
+  	allsort("32", 17);
 	std::ifstream file1("sorted.txt");
 	std::string temp1;
 	size_t t = 0;
@@ -56,7 +56,6 @@ SCENARIO("file_big", "file_big]"){
 	while(!file1.eof()) {
 		std::getline(file1, temp1);
 	}
-	REQUIRE(t == 1637032);
+	REQUIRE(t == 1637032+1);
   	file1.close();
 }
-*/

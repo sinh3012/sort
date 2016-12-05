5f7f0732587ff89779f3f022e69cdb87936abad8
Lair Sinh <assassin3012@mail.ru>
2016-12-05 19:49:33 +0300

    Update sort.hpp

--- include/sort.hpp
+++ include/sort.hpp
@@ -104,7 +104,7 @@ auto mergenew(size_t memory_, size_t piece_, std::string strtempfile) -> void
 
 auto allsort(std::string filename_, size_t memorymbyte_, std::string outfile = "sorted.txt") -> void
 {
-	size_t mem_ = memorymbyte_ * 1024 * 1024 / (2 * sizeof(std::string) + sizeof(short));
+	size_t mem_ = memorymbyte_ * 1024 * 1024 / (2 * sizeof(std::string) + sizeof(short) + 6);
 	std::cout << sizeof(std::string) << std::endl;
 	std::cout << 2*sizeof(std::string) << std::endl;
 	std::cout << sizeof(short) << std::endl;
